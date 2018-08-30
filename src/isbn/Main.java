9adcaed52c26591caed340c7d61b3cdee07be946
theodelvas7 <theodelvas7@gmail.com>
2018-08-30 21:20:52 +0200

    añadir guion
    he añadido un guíon :D

--- src/isbn/Main.java
+++ src/isbn/Main.java
@@ -24,7 +24,7 @@ public class Main {
         
         for (int i = 0; i < partidos.size(); i++) {
             
-            System.out.println( i + " " + partidos.get(i));
+            System.out.println( i + "-" + partidos.get(i));
         }
         
     }
